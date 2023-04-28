ced8c1a198b4b8a9c9f0012d697b0faae0a9c355
twinnytwin22 <>
2023-04-28 14:48:57 -0700

    update contract

--- lib/fatchVars.tsx
+++ lib/fatchVars.tsx
@@ -2,7 +2,7 @@ export const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_ID_TESTNET
 
 export const alchemyOptions = {method: 'GET',  cache: 'no-store' , headers: {accept: 'application/json'},};
 
-export const contract = '0xcd115615F392B24455229731CD82b436C4862366'
+export const contract = '0xB3187862F75608eD2720B4465BaB91bba6577cC6'
 
 export const tld = ".subport";
 
